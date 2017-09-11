b4b1f1a7edf8d01170d9877c8e3750bed3859d83
William Schoenell <wschoenell@gmail.com>
2017-09-11 14:56:48 -0300

    Added autopierchange code

--- README.rst
+++ README.rst
@@ -1,57 +1,34 @@
-chimera-template plugin
-=======================
+chimera-autopierchange plugin
+=============================
 
-This is a template plugin for the chimera observatory control system
+Automatically flip pier side for German Equatorial mounts in the chimera observatory control system
 https://github.com/astroufsc/chimera.
 
 Usage
 -----
 
-Rename chimera_template for your plugin name. It is important that the plugin
-name must start with chimera\_ to be found by chimera. Instruments and
-controllers must follow the standard ``chimera_(plugin_name)/(instruments|controllers)/(plugin).py``
-
-The class inside ``(plugin).py`` should be named Plugin (with CamelCase letters).
-
-For more info: https://github.com/astroufsc/chimera/blob/master/docs/site/chimerafordevs.rst#chimera-objects
-
+Install and configure `chimera-autopierchange` plugin as example below.
 
 Installation
 ------------
 
-Installation instructions. Dependencies, etc...
-
 ::
 
-   pip install -U chimera_template
-
-or
-
-::
-
-    pip install -U git+https://github.com/astroufsc/chimera-template.git
+   pip install -U https://github.com/astroufsc/chimera-autopierchange/archive/master.zip
 
 
 Configuration Example
 ---------------------
 
-Here goes an example of the configuration to be added on ``chimera.config`` file.
-
 ::
 
-    instrument:
-        name: model
-        type: Example
-
-
-Tested Hardware (for instruments)
----------------------------------
-
-This plugin was tested on these hardware:
-
-* Hardware example 1, model 2
-* Hardware example 2, model 3
-
+   - type: AutoPierChange
+     name: pier_change
+     telescope: /FakeTelescope/fake
+     dome: None  # If dome is set, controller calls syncWithTel() dome method after pierside flip
+     ha_flip: 0  # Flip hour angle
+     check_interval: 2 # Check telescope position every check_interval seconds
+     chimera_config: SYSTEM_CONFIG_DEFAULT_FILENAME # Path to chimera.config file
 
 Contact
 -------
@@ -60,4 +37,4 @@ For more information, contact us on chimera's discussion list:
 https://groups.google.com/forum/#!forum/chimera-discuss
 
 Bug reports and patches are welcome and can be sent over our GitHub page:
-https://github.com/astroufsc/chimera-template/
+https://github.com/astroufsc/chimera-autopierchange/
---
+++ chimera_autopierchange/controllers/autopierchange.py
@@ -0,0 +1,65 @@
+import time
+
+from chimera.core.callback import callback
+from chimera.core.chimeraobject import ChimeraObject
+from chimera.core.constants import SYSTEM_CONFIG_DEFAULT_FILENAME
+from chimera.core.manager import Manager
+from chimera.core.systemconfig import SystemConfig
+from chimera.interfaces.telescope import TelescopeStatus, TelescopePierSide
+from chimera.util.coord import CoordUtil
+
+
+class AutoPierChange(ChimeraObject):
+    __config__ = {
+        "site": "/Site/0",
+        "telescope": "/Telescope/0",
+        "dome": None,
+        "ha_flip": 0,
+        "check_interval": 2,  # seconds
+        "chimera_config": SYSTEM_CONFIG_DEFAULT_FILENAME
+    }
+
+    def __init__(self):
+        ChimeraObject.__init__(self)
+
+    def __start__(self):
+
+        self.setHz(1. / self["check_interval"])
+
+        self.sysconfig = SystemConfig.fromFile(self['chimera_config'])
+
+        self.localManager = Manager(self.sysconfig.chimera["host"], 9090)
+        self.site = self.getManager().getProxy(self["site"])
+        if self["dome"] is not None:
+            self.dome = self.getManager().getProxy(self["dome"])
+        else:
+            self.dome = None
+        self.pierPos = TelescopePierSide.UNKNOWN
+
+        @callback(self.localManager)
+        def slewComplete(position, status):
+            if status == TelescopeStatus.OK:
+                ha = CoordUtil.raToHa(position.ra, self.site.LST()).H
+                if ha < self["ha_flip"]:
+                    self.pierPos = TelescopePierSide.EAST
+                else:
+                    self.pierPos = TelescopePierSide.WEST
+                self.log.debug("Telescope moved to %s, LST is %s, HA is %s.", str(self.pierPos),
+                               str(self.site.LST()), str(position))
+
+        self.telescope = self.getManager().getProxy(self["telescope"])
+        self.telescope.slewComplete += slewComplete
+
+    def control(self):
+        if self.pierPos == TelescopePierSide.EAST:
+            ha = CoordUtil.raToHa(self.telescope.getRa(), self.site.LST()).H
+            if ha >= self["ha_flip"]:
+                self.log.debug("Flipping telescope pier...")
+                t0 = time.time()
+                self.telescope.slewToRaDec(self.telescope.getPositionRaDec())
+                if self.dome is not None:
+                    self.log.debug("Syncing dome...")
+                    self.dome.syncWithTel()
+                self.log.debug("Flipping telescope pier: took %3.2f s.", time.time() - t0)
+
+        return True
